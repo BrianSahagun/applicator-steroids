9b2e26f8566e426b4d2ccb764d303866f6325d39
Brian Dys Sahagun <briansahagun@gmail.com>
2018-03-01 09:53:43 +0800

    Feature: Prezo

--- prezo/functions.php
+++ prezo/functions.php
@@ -1,12 +1,11 @@
 <?php
 
+
+
+
+
 // Styles and Scripts
-
-
-
-
-
-function applicator_prezo()
+function applicator_prezo_cf()
 {
     $class_name_post_meta = get_post_meta( get_the_ID(), 'Applicator: Prezo', true );
     $class_name_post_meta_clean = substr( sanitize_title( preg_replace( '/\s+/', '', trim( $class_name_post_meta ) ) ), 0, 8 );
@@ -19,6 +18,8 @@ function applicator_prezo()
         'activated',
         'on',
         '1',
+        'run',
+        'go',
     );
     
     return ( in_array( $class_name_post_meta_clean, $on_terms, true ) );
@@ -31,7 +32,7 @@ function applicator_prezo()
 // ------------------------------------ Prezo Feature via Custom Fields – Inserted into the Body Class
 function applicator_prezo_cf_body_class( $classes )
 {
-    if ( applicator_prezo() )
+    if ( applicator_prezo_cf() )
     {
         $classes[] = 'prezo---a8r_f';
     }
@@ -45,7 +46,7 @@ add_filter( 'body_class', 'applicator_prezo_cf_body_class' );
 
 function applicator_steroid_prezo_styles_scripts()
 {
-    if ( applicator_prezo() )
+    if ( applicator_prezo_cf() )
     {
         wp_enqueue_style( 'applicator-prezo-style--default', plugin_dir_url( __FILE__ ). 'assets/css/default.css' );
         wp_enqueue_script( 'applicator-prezo-script-default', plugin_dir_url( __FILE__ ). 'assets/js/default.js', array( 'jquery' ), '1.0', true );
